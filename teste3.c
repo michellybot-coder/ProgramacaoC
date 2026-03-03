57f8bac2814c555e122b2dc95a9bbd9a644657c6
michellybot-coder <michellybot@gmail.com>
2026-03-03 14:06:11 -0300

    Add a C program to sum two numbers

---
+++ teste3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, soma;
+
+    //Lendo dois números do usuário
+printf("Digite o primeiro número: ");
+scanf("%d", &num1);
+
+printf("Digite o segundo número: ");
+scanf("%d", &num2);
+
+??Calculando o soma dos dois números
+soma = num1 + num2;
+
+//Exibindo o resultado
+printf("A soma de %d e %d é: %d\n", num1, num2, soma);
+
+return 0;
+
+}
